7cf7bd8156d9a58d09fae1aff1eb80fa5bfec90c
Sebastian <seba.kowal2001@gmail.com>
2023-10-29 14:44:59 +0100

    LogInPrzezObrazki_LogOutDoMainPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,27 @@
-'use client';
-import Link from "next/link";
-import { signOut, useSession } from 'next-auth/react';
-import { redirect } from 'next/navigation';
-import Header from "@/components/Header";
-export default function Home() {
+"use client";
+import useAuthModal from "../../hooks/useAuthModal";
+import { useUser } from "../../hooks/useUser";
+import React from "react";
+import { useRouter } from "next/navigation";
 
+export default function Home() {
+  const router = useRouter();
+  const authModal = useAuthModal();
+  const { user } = useUser();
+
+  const handleImageClick = (subject: string) => {
+    if (!user) {
+      authModal.onOpen();
+    } else {
+      router.push(
+        subject === "Stwórz Quiz za pomocą AI" ? "/Quized_by_AI" : "/that_quiz"
+      );
+    }
+  };
 
   return (
-
     <div className="bg-gray-100 dark:bg-gray-900 flex justify-center">
-
-      <main className="max-w-8xl mx-auto p-7 ">
+      <main className="max-w-8xl mx-auto p-7">
         <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {[
             "Stwórz Quiz za pomocą AI",
@@ -26,38 +37,22 @@ export default function Home() {
               key={subject}
               className="bg-white dark:bg-gray-900 p-4 rounded-lg shadow-md"
             >
-              {subject === "Stwórz Quiz za pomocą AI" ? (
-                <Link href={`/Quized_by_AI`}>
-                  <img
-                    src={`/images/AI.png`}
-                    alt={subject}
-                    className="w-120 mx-auto rounded-lg"
-                  />
-                </Link>
-              ) : (
-                <Link href={`/that_quiz`}>
-                  <img
-                    src={`/images/${subject}.png`}
-                    alt={subject}
-                    className="w-120 mx-auto rounded-lg"
-                  />
-                </Link>
-              )}
+              <a onClick={() => handleImageClick(subject)}>
+                <img
+                  src={
+                    subject === "Stwórz Quiz za pomocą AI"
+                      ? `/images/AI.png`
+                      : `/images/${subject}.png`
+                  }
+                  alt={subject}
+                  className="w-120 mx-auto rounded-lg"
+                />
+              </a>
               <p className="text-center">{subject}</p>
             </div>
           ))}
         </section>
-
       </main>
-
-
-
-
-
     </div>
   );
-
-
 }
-
-Home.requireAuth = true
--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,10 +1,10 @@
-'use client'
+"use client";
 import Link from "next/link";
 import React from "react";
 import useAuthModal from "../../../hooks/useAuthModal";
 import Button from "../Button";
 import { useRouter } from "next/navigation";
-import { useSupabaseClient } from '@supabase/auth-helpers-react';
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useUser } from "../../../hooks/useUser";
 import { toast } from "react-hot-toast";
 import { FaUserAlt } from "react-icons/fa";
@@ -14,9 +14,7 @@ interface HeaderProps {
   className?: string;
 }
 
-const Navbar = ({
-
-}) => {
+const Navbar = ({}) => {
   //const player = usePlayer();
   const router = useRouter();
   const authModal = useAuthModal();
@@ -28,27 +26,27 @@ const Navbar = ({
     const { error } = await supabaseClient.auth.signOut();
     //player.reset();
     router.refresh();
-
+    router.push("/");
     if (error) {
       toast.error(error.message);
     }
-  }
+  };
 
   return (
     <div className="sticky inset-x-0 top-0 bg-white dark:bg-gray-950 z-[20] h-fit border-b border-zinc-300">
       <div className="flex items-center justify-between py-2 px-8 mx-auto max-w-7xl">
-
-        <button onClick={() => router.push('/')} className="rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold transition-all hover:-translate-y-[2px] md:block dark:border-white">
+        <button
+          onClick={() => router.push("/")}
+          className="rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold transition-all hover:-translate-y-[2px] md:block dark:border-white"
+        >
           Quiz_app
         </button>
 
-
         {user ? (
           <div className="flex gap-x-4 items-center">
-
             {user.email}
             <Button
-              onClick={() => router.push('/account')}
+              onClick={() => router.push("/account")}
               className="bg-white"
             >
               <FaUserAlt />
@@ -61,8 +59,12 @@ const Navbar = ({
             </Button>
           </div>
         ) : (
-          <button onClick={authModal.onOpen}
-            className="rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold transition-all hover:-translate-y-[2px] md:block dark:border-white ml-4">Zaloguj</button>
+          <button
+            onClick={authModal.onOpen}
+            className="rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold transition-all hover:-translate-y-[2px] md:block dark:border-white ml-4"
+          >
+            Zaloguj
+          </button>
         )}
       </div>
     </div>
