fca728d08d93a924afdb950237bafc1c81b4fbe5
Sebastian <seba.kowal2001@gmail.com>
2024-01-05 17:54:52 +0100

    2 front option

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
   return (
     <div className="bg-gray-100 dark:bg-gray-900 flex justify-center">
       <main className="max-w-8xl mx-auto p-7">
-        <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
           {data
             .sort((a, b) =>
               a.description === "AI" ? -1 : b.description === "AI" ? 1 : 0
@@ -53,16 +53,16 @@ export default function Home() {
             .map((quiz: QuizData) => (
               <div
                 key={quiz.title}
-                className="bg-white dark:bg-gray-900 p-4 rounded-lg shadow-md"
+                className="bg-zinc-50 dark:bg-gray-900 p-4 pb-1 rounded-2xl shadow-md border border-gray-200"
               >
                 <a onClick={() => handleImageClick(quiz.description)}>
                   <img
                     src={quiz.logo}
                     alt={quiz.title}
-                    className="w-120 mx-auto rounded-lg"
+                    className="w-120 mx-auto rounded-2xl"
                   />
                 </a>
-                <p className="text-center">{quiz.title}</p>
+                <p className="mt-1 text-center font-serif font-semibold ">{quiz.title}</p>
               </div>
             ))}
         </section>
